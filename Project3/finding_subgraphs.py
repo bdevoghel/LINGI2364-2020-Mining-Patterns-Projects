103ee3181ae7085067f9a7b218e5dd194574472b
Brieuc de Voghel <bdev@telenet.be>
2020-04-22 16:37:54 +0200

    correcting heap, print, self methods

--- Project3/finding_subgraphs.py
+++ Project3/finding_subgraphs.py
@@ -22,12 +22,12 @@ from collections import Counter, OrderedDict, defaultdict
 from heapq import heappop, heappush, heapify
 from math import log2
 
-minScoreConf = 0.5
 
 class Heap:
     """
     Data structure easing the use of a min- or max-heap.
-    Stored items are 2-tuples with the first element being the ordering score.
+    Stored items are 3-tuples with the first 2 elements being 
+    the ordering confidence and support and the last element being the item.
     """
 
     def __init__(self, order="min"):
@@ -37,37 +37,38 @@ class Heap:
         heapify(self.heap)
 
     def push(self, item):
-        heappush(self.heap, (self.order * item[0], item[1]))
+        heappush(self.heap, (self.order * item[0], self.order * item[1], item[2]))
         self.size += 1
 
     def pop(self):
         pop = heappop(self.heap)
         self.size -= 1
-        return (pop[0] * self.order, pop[1])
+        return (pop[0] * self.order, pop[1] * self.order, pop[2])
 
     def pop_item(self):
-        return self.pop()[1]
+        return self.pop()[2]
 
     def is_empty(self):
         return self.size == 0
 
     def peek(self):
         if not self.is_empty():
-            return (self.heap[0][0] * self.order, self.heap[0][1])
+            return (self.heap[0][0] * self.order, self.heap[0][1] * self.order, self.heap[0][2])
 
-    def contains(self, score):
+    def contains_confidence(self, confidence):
         for item in self.heap:
-            if item[0] * self.order == score:
+            if item[0] * self.order == confidence:
                 return True
         return False
 
-    def containsSame(self, score, support):
+    def contains_confidence_and_support(self, confidence, support):
         for item in self.heap:
-            if item[0] * self.order == score:
-                item_support =  item[1][0]
-                if item_support == support:
-                    return True
+            if item[0] * self.order == confidence and item[1] * self.order == support:
+                return True
         return False
+
+    def get_all_sorted(self, reverse=False):
+        return [(item[0] * self.order, item[1] * self.order, item[2]) for item in sorted(self.heap, reverse=reverse)]
 
 
 class PatternGraphs:
@@ -82,7 +83,6 @@ class PatternGraphs:
         # The gid-subsets parameter in the pruning and store function will contain for each subset, all the occurrences
         # in which the examined pattern is present.
         self.gid_subsets = []
-
         self.database = database  # A graphdatabase instance: contains the data for the problem.
 
     def store(self, dfs_code, gid_subsets):
@@ -119,95 +119,68 @@ class FrequentPositiveAndNegativeGraphs(PatternGraphs):
         :param subsets: the subsets (train and/or test sets for positive and negative class) of graph ids.
         """
         super().__init__(database)
-        #self.patterns = []  # The patterns found in the end (as dfs codes represented by strings) with their cover (as a list of graph ids).
-        self.minsup = minsup
-        self.gid_subsets = subsets
 
-        self.k_best_score = Heap(order="min")  # min heap containting the currently k best solutions
-        #k_best_score.push((-sys.maxsize, (dfs_code, gid_subsets)))  # adding root with worst score
-        self.nb_different_scores = 0  # counts number of different scores there are in k_best_wracc, should always be <= k
+        # The most confident patterns found in the end (as dfs codes represented by strings) with their confidence and support
+        self.most_confident = Heap(order="min")
+
+        self.min_support = minsup
+        self.min_confidence = 0
+        self.gid_subsets = subsets
         self.k = k
-        # support heuristic values for BFS
-        self.minScoreConf = 0
 
     # Stores any pattern found that has not been pruned
     def store(self, dfs_code, gid_subsets):
-        #self.patterns.append((dfs_code, gid_subsets))
-        #add_sequence(dfs_code, gid_subsets, heap, nb_different_scores, k, score, minScoreConf)
-        self.nb_different_scores, self.minScoreConf = add_sequence(dfs_code, gid_subsets,
-                                self.k_best_score, self.nb_different_scores, self.k, score_confidence(self, gid_subsets), self.minScoreConf)
+        self.add_sequence(dfs_code, gid_subsets, self.confidence(gid_subsets))
 
     # Prunes any pattern that is not frequent in the positive and negative class
     def prune(self, gid_subsets):
         # first subset is the set of positive and negative ids
-        return (not satisfies_heuristic(self, gid_subsets)) #or (score_confidence(self, gid_subsets) < self.minScoreConf) #TO CHECK
+        return not self.satisfies_minsup(gid_subsets)  #or  self.confidence(gid_subsets) < self.min_confidence # TODO CHECK anti-monotonicity
 
-    # creates a column for a feature matrix
-    def create_fm_col(self, all_gids, subset_gids):
-        subset_gids = set(subset_gids)
-        bools = []
-        for i, val in enumerate(all_gids):
-            if val in subset_gids:
-                bools.append(1)
+    def confidence(self, gid_subsets):  # not anti-monotonic
+        pos_support = len(gid_subsets[0])
+        neg_support = len(gid_subsets[1])
+        confidence = pos_support / (pos_support + neg_support)
+        return confidence
+
+    def satisfies_minsup(self, gid_subsets):  # anti-monotonic
+        pos_support = len(gid_subsets[0])
+        neg_support = len(gid_subsets[1])
+        return pos_support + neg_support >= self.min_support
+
+    def add_sequence(self, dfs_code, gid_subsets, confidence):
+        support = len(gid_subsets[0]) + len(gid_subsets[1])
+        if self.k > 0:
+            if not self.most_confident.contains_confidence_and_support(confidence, support):  # if same confidence and support : sequence should be added without decrementing k
+                self.k -= 1
+            self.most_confident.push( (confidence, support, dfs_code) )
+
+        else:  # reached k different sequences in heap, only add if confidence is already present or better
+
+            if self.most_confident.contains_confidence_and_support(confidence, support):  # same confidence and support are already present : add dfs_code
+                self.most_confident.push( (confidence, support, dfs_code) )                
             else:
-                bools.append(0)
-        return bools
+                min_conf, min_support, __ = self.most_confident.peek()
+                if confidence > min_conf or (confidence == min_conf and support > min_support):  # dfs_code is more interesting than what already exists : replace worst dfs_code
+                    new_min_conf, new_min_support, __ = self.most_confident.peek()
+                    while new_min_conf == min_conf and new_min_support == min_support:
+                        self.most_confident.pop()  # removes worst confidence
+                        new_min_conf, new_min_support, __ = self.most_confident.peek()
+                    self.min_confidence = new_min_conf
+                    self.most_confident.push( (confidence, support, dfs_code) )
 
-    # return a feature matrix for each subset of examples, in which the columns correspond to patterns
-    # and the rows to examples in the subset.
-    def get_feature_matrices(self):
-        matrices = [[] for _ in self.gid_subsets]
-        for pattern, gid_subsets in self.patterns:
-            for i, gid_subset in enumerate(gid_subsets):
-                matrices[i].append(self.create_fm_col(self.gid_subsets[i], gid_subset))
-        return [numpy.array(matrix).transpose() for matrix in matrices]
-
-
-def score_confidence(self, gid_subsets):
-    #break
-    conf = len(gid_subsets[0]) / (len(gid_subsets[0]) + len(gid_subsets[1]))
-    return conf
+                else: # worse confidence : do not add dfs_code
+                    pass
 
 
-def satisfies_heuristic(self, gid_subsets):
-    #break
-    pos_support = len(gid_subsets[0])
-    neg_support = len(gid_subsets[1])
-    return pos_support + neg_support >= self.minsup
-
-
-def printOutput(conf, support, dfs_code):
-    print(dfs_code, end=' ')
-    print(conf, end=' ')
-    print(support)
-
-
-def add_sequence(dfs_code, gid_subsets, heap, nb_different_scores, k, score, minScoreConf):
-    support = len(gid_subsets[0]) + len(gid_subsets[1])
-    if nb_different_scores < k:
-        if not heap.containsSame(score, support):  #IDEM sequence should be added without decrementing k
-            nb_different_scores += 1
-        heap.push((score, (support, dfs_code)))
-
-    else:  # reached k sequences in heap, only add if score is already present or better
-        min_conf, (min_support, __) = heap.peek()
-        if heap.containsSame(score, support):   # score and support is already present
-            heap.push((score, (support, dfs_code)))
-        elif score > min_conf:  # score is more interesting than what already exists
-            new_min_conf, (new_support, __) = heap.peek()
-            while new_min_conf == min_conf and new_support == min_support:  #TODO RETIRER QUE AVEC SUPPORT IDEM
-                heap.pop()  # removes worst score
-                new_min_conf, (new_support, __) = heap.peek()
-            heap.push((score, (support, dfs_code)))
-
-            minScoreConf = new_min_conf
-    return nb_different_scores, minScoreConf
+def print_output(conf, support, dfs_code):
+    print(dfs_code, conf, support)
 
 
 def finding_subgraphs(database_file_name_pos, database_file_name_neg, k, minsup):
     """
-    Runs gSpan with the specified positive and negative graphs, finds all frequent subgraphs in the positive class
-    with a minimum positive support of minsup and prints them.
+    Runs gSpan with the specified positive and negative graphs, finds k most confident frequent subgraphs in the
+    positive class with a minimum support (positive and negative) of minsup and prints them.
     """
 
     if not os.path.exists(database_file_name_pos):
@@ -226,16 +199,9 @@ def finding_subgraphs(database_file_name_pos, database_file_name_neg, k, minsup)
 
     gSpan(task).run()  # Running gSpan
 
-    # Printing frequent patterns along with their positive support:
-	# TODO print k most confident
-
-    # print k best sequences
-    for item in sorted(task.k_best_score.heap, reverse=True):
-        printOutput(item[0], item[1][0], item[1][1])
-
-    #for pattern, gid_subsets in task.patterns:
-    #    pos_support = len(gid_subsets[0])  # This will have to be replaced by the confidence and support on both classes
-    #    print('{} {}'.format(pattern, pos_support))
+    # Printing k most confident patterns along with their confidence and total support:
+    for item in task.most_confident.get_all_sorted(reverse=True):
+        print_output(item[0], item[1], item[2])
 
 
 if __name__ == '__main__':
@@ -247,4 +213,3 @@ if __name__ == '__main__':
     # third parameter: size of topk
     # fourth parameter: minimum support
     finding_subgraphs(args[1], args[2], k=int(args[3]), minsup=int(args[4]))
-	# test with .\Datasets\molecules-small.pos .\Datasets\molecules-small.pos 5 10
