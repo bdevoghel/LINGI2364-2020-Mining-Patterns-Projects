fb90d9d64fe98c731660ab7e8ad9f65ce3d0be0a
Brieuc de Voghel <bdev@telenet.be>
2020-04-22 12:40:43 +0200

    color correction

--- Project3/test_output.py
+++ Project3/test_output.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 
 def test_finding_subgraph(recompute=True, del_output=True):
     if recompute:
@@ -7,10 +6,14 @@ def test_finding_subgraph(recompute=True, del_output=True):
     
     with open('fs_test.tmp', 'r') as test:
         test_lines = [line.strip() for line in test.readlines()]
-        with open('Examples/task1_small_5_5.txt', 'r') as real:
-            real_lines = [line.strip() for line in real.readlines()]
+    with open('Examples/task1_small_5_5.txt', 'r') as real:
+        real_lines = [line.strip() for line in real.readlines()]
 
-    print(f"\033[1;31;1mSolution has \t\t{len(real_lines)} lines,\n   while we have \t{len(test_lines)} lines\n\033[0;0;0m")
+    red = "[1;31;1m"
+    green = "[1;32;1m"
+
+    color = green if len(real_lines) == len(test_lines) else red
+    print(f"\033{color}Solution has \t\t{len(real_lines)} lines,\n   while we have \t{len(test_lines)} lines\n\033[0;0;0m")
 
     wrong_lines = [line not in real_lines for line in test_lines]
     missing_lines = [line not in test_lines for line in real_lines]
@@ -18,10 +21,12 @@ def test_finding_subgraph(recompute=True, del_output=True):
     wrong_lines_i = [(i if wrong_lines[i] else None) for i in range(len(wrong_lines))]
     missing_lines_i = [(i if missing_lines[i] else None) for i in range(len(missing_lines))]
 
-    
-    print(f"\033[1;31;1mWrong lines   : {sum(wrong_lines)}\033[0;0;0m\t(present in output but not in solution)")
+    color = green if sum(wrong_lines) == 0 else red
+    print(f"\033{color}Wrong lines   : {sum(wrong_lines)}\033[0;0;0m\t(present in output but not in solution)")
     print(f"\t{[i for i in wrong_lines_i if i is not None]}\n")
-    print(f"\033[1;31;1mMissing lines : {sum(missing_lines)}\033[0;0;0m\t(present in solution but missing in output)")
+
+    color = green if sum(missing_lines) == 0 else red
+    print(f"\033{color}Missing lines : {sum(missing_lines)}\033[0;0;0m\t(present in solution but missing in output)")
     print(f"\t{[i for i in missing_lines_i if i is not None]}\n")
 
     if del_output:
